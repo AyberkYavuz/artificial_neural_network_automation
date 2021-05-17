4ec920eaf676f151b0d6b875201132ef4b1e8086
hikmetyavuz <hikmet.yavuz@misli.com>
2021-05-17 18:17:20 +0300

    ANNClassificationHandler was updated.

--- artificial_neural_network_model_automation/classification_handler.py
+++ artificial_neural_network_model_automation/classification_handler.py
@@ -3,41 +3,58 @@ from keras.layers import Dense
 from keras.layers import Dropout
 
 
+class ANNClassificationHandlerConfig:
+    classification_type: str
+    number_of_inputs: int
+    number_of_hidden_layers: int
+    dropout: bool
+    number_of_categories: str
+    metric: object
+    batch_size: int
+    epochs: int
+
+
 class ANNClassificationHandler:
-    def __init__(self, classification_type, number_of_inputs, number_of_hidden_layers,
-                 dropout, number_of_categories, metric):
-        self.classification_type = classification_type
-        self.number_of_inputs = number_of_inputs
-        self.number_of_hidden_layers = number_of_hidden_layers
-        self.dropout = dropout
-        self.number_of_categories = number_of_categories
-        self.metric = metric
+    def __init__(self, ann_classification_handler_config: ANNClassificationHandlerConfig):
+        self.__classification_type = ann_classification_handler_config.classification_type
+        self.__number_of_inputs = ann_classification_handler_config.number_of_inputs
+        self.__number_of_hidden_layers = ann_classification_handler_config.number_of_hidden_layers
+        self.__dropout = ann_classification_handler_config.dropout
+        self.__number_of_categories = ann_classification_handler_config.number_of_categories
+        self.__metric = ann_classification_handler_config.metric
+        self.__batch_size = ann_classification_handler_config.batch_size
+        self.__epochs = ann_classification_handler_config.epochs
+        self.__classifier = self.design_neural_network()
 
     def design_neural_network(self):
         # Initialising the ANN
         classifier = Sequential()
         # Adding input layer and first hidden layer
-        classifier.add(Dense(self.number_of_inputs, input_dim=self.number_of_inputs, activation='relu'))
-        if self.dropout:
+        classifier.add(Dense(self.__number_of_inputs, input_dim=self.__number_of_inputs, activation='relu'))
+        if self.__dropout:
             # adding the input layer and the first hidden layer with dropout
             classifier.add(Dropout(rate=0.1))
         # adding other hidden layers
-        for i in range(0, self.number_of_hidden_layers-1):
-            classifier.add(Dense(self.number_of_inputs + 5, activation='relu'))
-            if self.dropout:
+        for i in range(0, self.__number_of_hidden_layers-1):
+            classifier.add(Dense(self.__number_of_inputs + 5, activation='relu'))
+            if self.__dropout:
                 # adding dropout layer
                 classifier.add(Dropout(rate=0.1))
 
         # adding output layer
-        if self.classification_type == "binary":
-            classifier.add(Dense(self.number_of_categories, activation='sigmoid'))
+        if self.__classification_type == "binary":
+            classifier.add(Dense(self.__number_of_categories, activation='sigmoid'))
         else:
-            classifier.add(Dense(self.number_of_categories, activation='softmax'))
+            classifier.add(Dense(self.__number_of_categories, activation='softmax'))
 
         # compile classifier
-        if self.classification_type == "binary":
-            classifier.compile(loss='binary_crossentropy', optimizer="adam", metrics=[self.metric])
+        if self.__classification_type == "binary":
+            classifier.compile(loss='binary_crossentropy', optimizer="adam", metrics=[self.__metric])
         else:
-            classifier.compile(loss='categorical_crossentropy', optimizer="adam", metrics=[self.metric])
+            classifier.compile(loss='categorical_crossentropy', optimizer="adam", metrics=[self.__metric])
 
         return classifier
+
+    def train_neural_network(self, X, y):
+        classification_model = self.__classifier.fit(X, y, batch_size=self.__batch_size, epochs=self.__epochs)
+        return classification_model
