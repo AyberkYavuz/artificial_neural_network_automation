d8c379d36fe63bd9da1c86c4c1a1ca486c48d295
hikmetyavuz <hikmet.yavuz@misli.com>
2021-05-21 13:06:32 +0300

    train_neural_network()'s  docstring was updated.

--- artificial_neural_network_model_automation/classification_handler.py
+++ artificial_neural_network_model_automation/classification_handler.py
@@ -72,4 +72,26 @@ class ANNClassificationHandler:
 
     @execution_time
     def train_neural_network(self, X, y):
+        """Trains the designed Keras classifier.
+
+        Args:
+          X: Feature set. It could be:
+          - A Numpy array (or array-like), or a list of arrays
+            (in case the model has multiple inputs).
+          - A TensorFlow tensor, or a list of tensors
+            (in case the model has multiple inputs).
+          - A dict mapping input names to the corresponding array/tensors,
+            if the model has named inputs.
+          - A `tf.data` dataset. Should return a tuple
+            of either `(inputs, targets)` or
+            `(inputs, targets, sample_weights)`.
+          - A generator or `keras.utils.Sequence` returning `(inputs, targets)`
+            or `(inputs, targets, sample_weights)`.
+          y: labels. Like the input data `X`,
+          it could be either Numpy array(s) or TensorFlow tensor(s).
+          It should be consistent with `X` (you cannot have Numpy inputs and
+          tensor targets, or inversely). If `X` is a dataset, generator,
+          or `keras.utils.Sequence` instance, `y` should
+          not be specified (since targets will be obtained from `X`).
+        """
         self.classifier.fit(X, y, batch_size=self.__batch_size, epochs=self.__epochs)
