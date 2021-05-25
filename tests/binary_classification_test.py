eda08dd7e47333e400208e4482ffa42285251a12
hikmetyavuz <hikmet.yavuz@misli.com>
2021-05-25 16:31:29 +0300

    optimizer test.

--- tests/binary_classification_test.py
+++ tests/binary_classification_test.py
@@ -30,7 +30,10 @@ ann_classification_handler_config.classification_type = "binary"
 ann_classification_handler_config.neural_network_architecture = [60, 65, 70, 65, 1]
 ann_classification_handler_config.hidden_layers_activation_function = "relu"
 ann_classification_handler_config.dropout_dictionary = {"dropout": False, "dropout_rate": 0.01}
-ann_classification_handler_config.optimizer = "adam"
+# ann_classification_handler_config.optimizer = "adam"
+from tensorflow.keras import optimizers
+optimizer = optimizers.SGD(learning_rate=0.02)
+ann_classification_handler_config.optimizer = optimizer
 ann_classification_handler_config.metric = "accuracy"
 ann_classification_handler_config.batch_size = 10
 ann_classification_handler_config.epochs = 50
