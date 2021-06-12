036d75cc29a26dd5eec240e0d542dbb669325b8d
hikmetyavuz <hikmet.yavuz@misli.com>
2021-06-12 13:03:50 +0300

    binary_classification_randomized_search_test.py module was updated.

--- tests/binary_classification_randomized_search_test.py
+++ tests/binary_classification_randomized_search_test.py
@@ -33,54 +33,50 @@ optimizer3 = Adam(learning_rate=0.02)
 optimizer4 = SGD()
 optimizer5 = SGD(learning_rate=0.02)
 
+classification_type = "binary"
+scoring = "roc_auc"
+neural_network_architecture_list = [
+    [60, 65, 65, 1],
+    [60, 70, 70, 1],
+    [60, 80, 1],
+    [60, 90, 1],
+    [60, 85, 1],
+    [60, 85, 85, 1],
+    [60, 85, 85, 85, 1],
+    [60, 75, 75, 75, 1],
+    [60, 60, 1],
+    [60, 60, 60, 1],
+    [60, 60, 60, 60, 1],
+    [60, 60, 60, 60, 60, 1],
+    [60, 80, 70, 1],
+    [60, 90, 80, 70, 1],
+]
+hidden_layers_activation_function_list = ["relu", "sigmoid", "tanh", "selu", "elu"]
+dropout_rate_list = [None, 0.0001, 0.001, 0.01, 0.02, 0.03, 0.04]
+optimizer_list = [optimizer1, optimizer2, optimizer3, optimizer4, optimizer5]
+metric_list = ["AUC", "Recall", "Precision"]
+batch_size_list = [10, 20, 30, 40, 50]
+epochs_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
 neural_network_config_list_dict = {
-    "classification_type": "binary",
-    "scoring": "roc_auc",
-    "neural_network_architecture_list": [
-        [60, 65, 65, 1],
-        [60, 70, 70, 1],
-        [60, 80, 1],
-        [60, 90, 1],
-        [60, 85, 1],
-        [60, 85, 85, 1],
-        [60, 85, 85, 85, 1],
-        [60, 75, 75, 75, 1],
-        [60, 60, 1],
-        [60, 60, 60, 1],
-        [60, 60, 60, 60, 1],
-        [60, 60, 60, 60, 60, 1],
-        [60, 80, 70, 1],
-        [60, 90, 80, 70, 1]
-    ],
-    "hidden_layers_activation_function_list": [
-        "relu",
-        "sigmoid",
-        "tanh",
-        "selu",
-        "elu"
-    ],
-    "dropout_rate_list": [
-        None,
-        0.0001,
-        0.001,
-        0.01,
-        0.02,
-        0.03,
-        0.04
-    ],
-    "optimizer_list": [optimizer1, optimizer2, optimizer3, optimizer4, optimizer5],
-    "metric_list": ["AUC", "Recall", "Precision"],
-    "batch_size_list": [10, 20, 30, 40, 50],
-    "epochs_list": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    "classification_type": classification_type,
+    "scoring": scoring,
+    "neural_network_architecture_list": neural_network_architecture_list,
+    "hidden_layers_activation_function_list": hidden_layers_activation_function_list,
+    "dropout_rate_list": dropout_rate_list,
+    "optimizer_list": optimizer_list,
+    "metric_list": metric_list,
+    "batch_size_list": batch_size_list,
+    "epochs_list": epochs_list
 }
 
 ann_classification_randomized_search_config = ANNClassificationRandomizedSearchConfig(neural_network_config_list_dict)
 
 
 ann_classification_randomized_search = ANNClassificationRandomizedSearch(ann_classification_randomized_search_config, n_jobs=-1)
-ann_classification_randomized_search.fit(X, encoded_Y)  # Method Execution Time : 6.32 seconds
+ann_classification_randomized_search.fit(X, encoded_Y)  # Method Execution Time : 0.12 minutes
 
 # parallelism test
 ann_classification_randomized_search_2 = ANNClassificationRandomizedSearch(ann_classification_randomized_search_config,
                                                                            n_iter=100, n_jobs=-1)
-ann_classification_randomized_search_2.fit(X, encoded_Y)  # Method Execution Time : 27.46 seconds
+ann_classification_randomized_search_2.fit(X, encoded_Y)  # Method Execution Time : 0.39 minutes
