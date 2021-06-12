adb37865e775b6ab6a8d905ace20b72caf46621e
hikmetyavuz <hikmet.yavuz@misli.com>
2021-06-12 11:38:13 +0300

    threshold argument was updated.

--- helper/classification_handler_helper.py
+++ helper/classification_handler_helper.py
@@ -129,18 +129,18 @@ def get_label_based_on_thresold(x, thresold):
     return result
 
 
-def get_predictions_from_dummy_prob_matrix(dummy_prob_matrix, prediction_column_names, thresold=0.5):
+def get_predictions_from_dummy_prob_matrix(dummy_prob_matrix, prediction_column_names, threshold=0.5):
     """Returns single predictions column for target category dummy probability variables.
 
     Args:
       dummy_prob_matrix: Dummy probability variables for target categories.
       prediction_column_names: Target categories.
-      thresold: thresold which will be used for labeling.
+      threshold: thresold which will be used for labeling.
     """
     dummy_prob_matrix_df = pd.DataFrame(dummy_prob_matrix, columns=prediction_column_names)
     for column in prediction_column_names:
         dummy_prob_matrix_df[column] = dummy_prob_matrix_df[column].apply(
-            lambda x: get_label_based_on_thresold(x, thresold=thresold))
+            lambda x: get_label_based_on_thresold(x, thresold=threshold))
 
     dummy_y_train_pred = dummy_prob_matrix_df.to_numpy()
     predictions = argmax(dummy_y_train_pred, axis=1)
