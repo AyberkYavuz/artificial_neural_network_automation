84968bf5a75fcc0fda6529d343ccfa81ed934bc9
hikmetyavuz <hikmet.yavuz@misli.com>
2021-06-12 13:10:14 +0300

    multi_class_classification_randomized_search_test.py module was updated.

--- tests/multi_class_classification_randomized_search_test.py
+++ tests/multi_class_classification_randomized_search_test.py
@@ -11,46 +11,42 @@ target = data.target
 
 
 # creating ANNClassificationRandomizedSearchConfig object
+classification_type = "multiclass"
+neural_network_architecture_list = [
+    [4, 9, 12, 9, 3],
+    [4, 15, 9, 3],
+    [4, 25, 3],
+    [4, 10, 10, 10, 10, 3],
+    [4, 8, 16, 8, 3],
+    [4, 16, 16, 3],
+    [4, 4, 4, 4, 4, 3],
+    [4, 50, 25, 3],
+    [4, 15, 15, 16, 3],
+    [4, 12, 12, 12, 3]
+]
+hidden_layers_activation_function_list = ["relu", "sigmoid", "tanh", "selu", "elu"]
+dropout_rate_list = [None, 0.0001, 0.001, 0.01, 0.02, 0.03, 0.04]
+
 optimizer1 = Adam()
 optimizer2 = Adam(learning_rate=0.01)
 optimizer3 = Adam(learning_rate=0.02)
 optimizer4 = SGD()
 optimizer5 = SGD(learning_rate=0.02)
+optimizer_list = [optimizer1, optimizer2, optimizer3, optimizer4, optimizer5]
+
+metric_list = ["AUC", "Recall", "Precision"]
+batch_size_list = [10, 20, 30, 40, 50]
+epochs_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 neural_network_config_list_dict = {
-    "classification_type": "multiclass",
-    "neural_network_architecture_list": [
-        [4, 9, 12, 9, 3],
-        [4, 15, 9, 3],
-        [4, 25, 3],
-        [4, 10, 10, 10, 10, 3],
-        [4, 8, 16, 8,  3],
-        [4, 16, 16, 3],
-        [4, 4, 4, 4, 4, 3],
-        [4, 50, 25, 3],
-        [4, 15, 15, 16, 3],
-        [4, 12, 12, 12, 3]
-    ],
-    "hidden_layers_activation_function_list": [
-        "relu",
-        "sigmoid",
-        "tanh",
-        "selu",
-        "elu"
-    ],
-    "dropout_rate_list": [
-        None,
-        0.0001,
-        0.001,
-        0.01,
-        0.02,
-        0.03,
-        0.04
-    ],
-    "optimizer_list": [optimizer1, optimizer2, optimizer3, optimizer4, optimizer5],
-    "metric_list": ["AUC", "Recall", "Precision"],
-    "batch_size_list": [10, 20, 30, 40, 50],
-    "epochs_list": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    "classification_type": classification_type,
+    "neural_network_architecture_list": neural_network_architecture_list,
+    "hidden_layers_activation_function_list": hidden_layers_activation_function_list,
+    "dropout_rate_list": dropout_rate_list,
+    "optimizer_list": optimizer_list,
+    "metric_list": metric_list,
+    "batch_size_list": batch_size_list,
+    "epochs_list": epochs_list
 }
 
 ann_classification_randomized_search_config = ANNClassificationRandomizedSearchConfig(neural_network_config_list_dict)
