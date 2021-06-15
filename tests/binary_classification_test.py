f34d46d18d0f70b33189de731257a1148bfb17ff
hikmetyavuz <hikmet.yavuz@misli.com>
2021-06-15 11:58:42 +0300

    binary_classification_test.py was updated.

--- tests/binary_classification_test.py
+++ tests/binary_classification_test.py
@@ -1,33 +1,22 @@
-from pandas import read_csv
-from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 from artificial_neural_network_model_automation.classification_handler import ANNClassificationHandlerConfig
 from artificial_neural_network_model_automation.classification_handler import ANNClassificationHandler
 from sklearn.metrics import classification_report
 from helper.data_path_handler import get_data_path
 
-data_name = "sonar.csv"
-# please define your os_type (mac, windows, linux)
-data_path = get_data_path(data_name, directory_name="data", os_type="mac")
+# loading data
+data = load_breast_cancer()
+features = data.data
+target = data.target
 
+# splitting data
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.33, random_state=42)
 
-# load dataset
-dataframe = read_csv(data_path, header=None)
-dataset = dataframe.values
-
-# split into input (X) and output (Y) variables
-X = dataset[:, 0:60].astype(float)
-print(X.shape)
-Y = dataset[:, 60]
-print(Y.shape)
-
-# encode class values as integers
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
 # designing neural network
 
 neural_network_config = {"classification_type": "binary",
-                         "neural_network_architecture": [60, 65, 65, 1],
+                         "neural_network_architecture": [30, 40, 40, 1],
                          "hidden_layers_activation_function": "relu",
                          "optimizer": "adam",
                          "metric": "Recall",
@@ -38,12 +27,14 @@ ann_classification_handler_config = ANNClassificationHandlerConfig(neural_networ
 ann_classification_handler = ANNClassificationHandler(ann_classification_handler_config)
 
 # save the plot of classifier architecture
-png_path = get_data_path("neural_network_architecture.png", "neural_network_model_architecture_plots", os_type="mac")
+png_path = get_data_path("neural_network_architecture_breast_cancer_data.png",
+                         "neural_network_model_architecture_plots",
+                         os_type="mac")
 ann_classification_handler.save_classifier_architecture_plot(png_path)
 # training neural network
-ann_classification_handler.train_neural_network(X, encoded_Y)
+ann_classification_handler.train_neural_network(X_train, y_train)
 # making predictions
-y_pred = ann_classification_handler.get_predictions(X)
+y_pred = ann_classification_handler.get_predictions(X_test)
 # classification report
-print(classification_report(encoded_Y, y_pred, target_names=encoder.classes_))
+print(classification_report(y_test, y_pred))
 
