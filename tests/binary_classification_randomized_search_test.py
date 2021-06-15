1a2352d288ab67da40f736c7e887806b2c57d665
hikmetyavuz <hikmet.yavuz@misli.com>
2021-06-15 13:51:44 +0300

    binary_classification_randomized_search_test.py was updated.

--- tests/binary_classification_randomized_search_test.py
+++ tests/binary_classification_randomized_search_test.py
@@ -1,30 +1,14 @@
-from pandas import read_csv
-from sklearn.preprocessing import LabelEncoder
-from helper.data_path_handler import get_data_path
+from sklearn.datasets import load_breast_cancer
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers import SGD
 from artificial_neural_network_model_automation.hyper_parameter_tuning import ANNClassificationRandomizedSearchConfig
 from artificial_neural_network_model_automation.hyper_parameter_tuning import ANNClassificationRandomizedSearch
 
-data_name = "sonar.csv"
-# please define your os_type (mac, windows, linux)
-data_path = get_data_path(data_name, directory_name="data", os_type="mac")
+# loading data
+data = load_breast_cancer()
+features = data.data
+target = data.target
 
-
-# load dataset
-dataframe = read_csv(data_path, header=None)
-dataset = dataframe.values
-
-# split into input (X) and output (Y) variables
-X = dataset[:, 0:60].astype(float)
-print(X.shape)
-Y = dataset[:, 60]
-print(Y.shape)
-
-# encode class values as integers
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
 
 # creating ANNClassificationRandomizedSearchConfig object
 optimizer1 = Adam()
@@ -36,22 +20,13 @@ optimizer5 = SGD(learning_rate=0.02)
 classification_type = "binary"
 scoring = "roc_auc"
 neural_network_architecture_list = [
-    [60, 65, 65, 1],
-    [60, 70, 70, 1],
-    [60, 80, 1],
-    [60, 90, 1],
-    [60, 85, 1],
-    [60, 85, 85, 1],
-    [60, 85, 85, 85, 1],
-    [60, 75, 75, 75, 1],
-    [60, 60, 1],
-    [60, 60, 60, 1],
-    [60, 60, 60, 60, 1],
-    [60, 60, 60, 60, 60, 1],
-    [60, 80, 70, 1],
-    [60, 90, 80, 70, 1]
+    [30, 40, 40, 1],
+    [30, 30, 1],
+    [30, 50, 1],
+    [30, 70, 1],
+    [30, 80, 1]
 ]
-hidden_layers_activation_function_list = ["relu", "sigmoid", "tanh", "selu", "elu"]
+hidden_layers_activation_function_list = ["relu", "selu", "elu", "sigmoid"]
 dropout_rate_list = [None, 0.0001, 0.001, 0.01, 0.02, 0.03, 0.04]
 optimizer_list = [optimizer1, optimizer2, optimizer3, optimizer4, optimizer5]
 metric_list = ["AUC", "Recall", "Precision"]
@@ -73,10 +48,7 @@ neural_network_config_list_dict = {
 ann_classification_randomized_search_config = ANNClassificationRandomizedSearchConfig(neural_network_config_list_dict)
 
 
-ann_classification_randomized_search = ANNClassificationRandomizedSearch(ann_classification_randomized_search_config, n_jobs=-1)
-ann_classification_randomized_search.fit(X, encoded_Y)  # Method Execution Time : 0.12 minutes
+ann_classification_randomized_search = ANNClassificationRandomizedSearch(ann_classification_randomized_search_config,
+                                                                         n_iter=100, n_jobs=-1)
+ann_classification_randomized_search.fit(features, target)
 
-# parallelism test
-ann_classification_randomized_search_2 = ANNClassificationRandomizedSearch(ann_classification_randomized_search_config,
-                                                                           n_iter=100, n_jobs=-1)
-ann_classification_randomized_search_2.fit(X, encoded_Y)  # Method Execution Time : 0.39 minutes
